1132ca8b3cd2d20b1af370a7cbe79136decf2c27
MAC Book <macbook@192.168.1.4>
2026-04-09 22:52:45 +0530

    fix render port

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ app.get('/', (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(5000, () => console.log('Server running on port 5000'));
+    const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log('DB connection error:', err));
   })
   .catch((err) => console.log('DB connection error:', err));
